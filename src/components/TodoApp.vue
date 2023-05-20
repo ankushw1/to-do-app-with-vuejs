4cfbdf8bac51781c072660b9fc196443ddd36a27
Ankush Waghmare <waghmareankush050@gmail.com>
2023-05-20 23:11:41 +0530

    adding task input fields

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='container'>
+  <h2 class='text-center mt-5'>Todo App</h2>
+
+  <div class='d-flex'>
+  <input type='text' class='form-control' placeholder='Enter task'>
+  <button class='btn btn-warning rounded-0'>Add Task</button>
+  </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+    name:'HelloWorld',
+    props:{
+        msg:String
+    }
+}
+</script>
+
+<style scoped>
+</style>
