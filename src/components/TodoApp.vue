979aa2a6fd1b95453da2b5271137f4acf9280dd1
Ankush Waghmare <waghmareankush050@gmail.com>
2023-05-20 23:31:46 +0530

    adding table

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -6,6 +6,33 @@
   <input type='text' class='form-control' placeholder='Enter task'>
   <button class='btn btn-warning rounded-0'>Add Task</button>
   </div>
+
+
+  <table class="table table-bordered mt-5">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Staus</th>
+      <th class='text-center' scope="col">Edit</th>
+      <th class='text-center' scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>mock final</th>
+      <td>To-do</td>
+            <td><div class='text-center'>
+              <span class="fa fa-trash pointer"></span>
+            </div></td>
+            <td><div class='text-center'>
+              <span class="fa fa-pen pointer"></span>
+            </div></td>
+    </tr>
+  </tbody>
+</table>
+
+
+
   </div>
 
 </template>
