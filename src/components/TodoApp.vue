c57d2e1ac17ccfa8b27aae2a214e2dd7562d47ab
Ankush Waghmare <waghmareankush050@gmail.com>
2023-05-21 11:50:36 +0530

    adding separate folder for separate categories

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -4,15 +4,18 @@
 
     <div class="d-flex">
       <input v-model="task" type="text" class="form-control" placeholder="Enter task" />
-      <select v-model="category" class="form-control ml-2">
-        <option value="Personal">Personal</option>
-        <option value="Business">Business</option>
-        <option value="Study">Study</option>
+      <select v-model="selectedCategory" class="form-control ml-2">
+        <option value="" disabled>Select category</option>
+        <option v-for="category in categories" :value="category" :key="category">{{ category }}</option>
       </select>
       <button @click="submitTask" class="btn btn-warning rounded-0">Add Task</button>
     </div>
 
-    <table class="table table-bordered mt-5">
+    <div class="d-flex mt-4">
+      <div v-for="category in categories" :key="category" @click="selectCategory(category)" :class="{ 'folder-active': category === selectedCategory }" class="folder">{{ category }}</div>
+    </div>
+
+    <table v-if="selectedCategory" class="table table-bordered mt-5">
       <thead>
         <tr>
           <th scope="col">Task</th>
@@ -23,23 +26,23 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(task, index) in tasks" :key="index">
+        <tr v-for="(task, index) in categorizedTasks[selectedCategory]" :key="index">
           <td>
             <span :class="{ 'line-through': task.status === 'Finished' }">{{ task.name }}</span>
           </td>
           <td style="width: 120px">
-            <span class="pointer" @click="statusUpdate(index)">{{ task.status }}</span>
+            <span class="pointer" @click="statusUpdate(task)">{{ task.status }}</span>
           </td>
           <td>
             {{ task.category }}
           </td>
           <td>
-            <div class="text-center" @click="editTask(index)">
+            <div class="text-center" @click="editTask(task)">
               <span class="fa fa-pen pointer"></span>
             </div>
           </td>
           <td>
-            <div class="text-center" @click="deleteTask(index)">
+            <div class="text-center" @click="deleteTask(task)">
               <span class="fa fa-trash pointer"></span>
             </div>
           </td>
@@ -48,6 +51,7 @@
     </table>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -59,54 +63,72 @@ export default {
   data() {
     return {
       task: '',
-      category: 'Personal',
+      selectedCategory: null,
       editedTask: null,
       availableStatuses: ['To-do', 'In-progress', 'Finished'],
-      tasks: [
-        {
-          name: 'ankush want to give test',
-          status: 'To-do',
-          category: 'Personal',
-        },
-      ],
+      categories: ['Personal', 'Business', 'Study'],
+      categorizedTasks: {
+        Personal: [
+          {
+            name: 'ankush wants to give a test',
+            status: 'To-do',
+            category: 'Personal',
+          },
+        ],
+        Business: [],
+        Study: [],
+      },
     };
   },
 
   methods: {
     submitTask() {
-      if (this.task.length === 0) return;
+      if (this.task.length === 0 || !this.selectedCategory) return;
 
       if (this.editedTask === null) {
-        this.tasks.push({
+        const newTask = {
           name: this.task,
           status: 'To-do',
-          category: this.category,
-        });
+          category: this.selectedCategory,
+        };
+        this.categorizedTasks[this.selectedCategory].push(newTask);
       } else {
-        this.tasks[this.editedTask].name = this.task;
-        this.tasks[this.editedTask].category = this.category;
+        const editedTask = this.categorizedTasks[this.selectedCategory][this.editedTask];
+        editedTask.name = this.task;
+        editedTask.category = this.selectedCategory;
         this.editedTask = null;
       }
 
       this.task = '';
     },
 
-    deleteTask(index) {
-      this.tasks.splice(index, 1);
+    deleteTask(task) {
+      const categoryTasks = this.categorizedTasks[task.category];
+      const index = categoryTasks.indexOf(task);
+      categoryTasks.splice(index, 1);
       alert('To-do task deleted successfully!!!');
     },
 
-    editTask(index) {
-      this.task = this.tasks[index].name;
-      this.category = this.tasks[index].category;
-      this.editedTask = index;
+    editTask(task) {
+      this.task = task.name;
+      this.selectedCategory = task.category;
+      this.editedTask = this.categorizedTasks[task.category].indexOf(task);
     },
 
-    statusUpdate(index) {
-      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
-      if (++newIndex > 2) newIndex = 0;
-      this.tasks[index].status = this.availableStatuses[newIndex];
+    statusUpdate(task) {
+      const newIndex = this.availableStatuses.indexOf(task.status) + 1;
+      task.status = this.availableStatuses[newIndex % this.availableStatuses.length];
     },
+
+  selectCategory(category) {
+  if (this.selectedCategory === category) {
+    this.selectedCategory = '';
+  } else {
+    this.selectedCategory = category;
+  }
+},
+
+
   },
 };
 </script>
@@ -118,5 +140,20 @@ export default {
 
 .line-through {
   text-decoration: line-through;
+}
+
+.folder {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 50px;
+  background-color: lightgray;
+  margin-right: 10px;
+  cursor: pointer;
+}
+
+.folder-active {
+  background-color: lightblue;
 }
 </style>
